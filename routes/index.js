b6dd7e5f15366bc14f43b75526e66fc3678db544
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-10-08 20:17:45 +0530

    db

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var MongoClient = require("mongodb").MongoClient;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,6 +9,8 @@ router.get("/", function (req, res, next) {
 
 router.post("/submit", (req, res) => {
   console.log(req.body);
+  MongoClient.connect("mongodb://localhost:27017");
+  res.send("<h1>Got IT</h1>");
 });
 
 module.exports = router;
